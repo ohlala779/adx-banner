1e0f31c9ed54c32d7b55c1458fd51cc7f17d2257
ohlala779 <regdomainkris@gmail.com>
2025-07-19 00:41:12 -0700

    Update 300x100.php

--- 300x100.php
+++ 300x100.php
@@ -1,2 +1,22 @@
-<script async data-cfasync="false" data-clbaid="" src="//clammyendearedkeg.com/bn.js"></script>
-<div data-cl-spot="2058133"></div>
+jQuery(document).ready(function($) {
+    // Tạo mã quảng cáo
+    var adContainer = document.createElement('div');
+    adContainer.className = 'adsbanner1';
+
+    var script = document.createElement('script');
+    script.async = true;
+    script.setAttribute("data-cfasync", "false");
+    script.setAttribute("data-clbaid", "");
+    script.src = "//clammyendearedkeg.com/bn.js";
+
+    var div = document.createElement('div');
+    div.setAttribute("data-cl-spot", "2058133");
+
+    adContainer.appendChild(script);
+    adContainer.appendChild(div);
+
+    var headerElements = document.getElementsByClassName('header-float');
+    if (headerElements.length > 0) {
+        headerElements[0].appendChild(adContainer);
+    }
+});
