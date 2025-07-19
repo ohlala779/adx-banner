84997b50e1d86344df42dd1bfe01de858d38525c
ohlala779 <regdomainkris@gmail.com>
2025-07-19 00:21:41 -0700

    Update and rename 300x100.html to 300x100.js

---
+++ 300x100.js
@@ -0,0 +1,12 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.setAttribute('data-cfasync', 'false');
+  script.setAttribute('data-clbaid', '');
+  script.src = '//clammyendearedkeg.com/bn.js';
+  document.head.appendChild(script);
+
+  var div = document.createElement('div');
+  div.setAttribute('data-cl-spot', '2058133');
+  document.body.appendChild(div);
+})();
