f050a2598d82f008d2f40bbd88e36d800dc21ac9
ohlala779 <regdomainkris@gmail.com>
2025-07-19 00:06:41 -0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+(function() {
+  function loadScript(url) {
+    var script = document.createElement('script');
+    script.setAttribute('data-cfasync', 'false');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild(script);
+  }
+
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
+})();
