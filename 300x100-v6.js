b76aa29e165520a7f165d8a3893882a802178189
ohlala779 <regdomainkris@gmail.com>
2025-07-19 01:28:08 -0700

    Update 300x100-v7.js

--- 300x100-v6.js
+++ 300x100-v6.js
@@ -1,23 +1,30 @@
 jQuery(document).ready(function($) {
     $.get("https://www.cloudflare.com/cdn-cgi/trace", function(data) {
         var lines = data.split('\n');
-        var ip = lines.find(line => line.startsWith('ip=')).split('=')[1];
         var country = lines.find(line => line.startsWith('loc=')).split('=')[1];
-        if (country.trim() === "VN") {
-            var script = document.createElement('script');
-            script.async = true;
-            script.setAttribute('data-cfasync', 'false');
-            script.setAttribute('data-clbaid', '');
-            script.src = '//clammyendearedkeg.com/bn.js';
-
+        console.log("Detected country:", country);
+        
+        // Thử bỏ điều kiện hoặc test luôn
+        if (country.trim() === "VN") { 
             var adsElements = document.getElementsByClassName('header-float');
             if (adsElements.length > 0) {
+                var script = document.createElement('script');
+                script.async = true;
+                script.setAttribute('data-cfasync', 'false');
+                script.setAttribute('data-clbaid', '');
+                script.src = '//clammyendearedkeg.com/bn.js';
+
                 adsElements[0].appendChild(script);
 
                 var adDiv = document.createElement('div');
                 adDiv.setAttribute('data-cl-spot', '2058133');
                 adsElements[0].appendChild(adDiv);
+                console.log("Script and div appended");
+            } else {
+                console.warn("Element .header-float not found");
             }
+        } else {
+            console.warn("Country not VN, banner not loaded");
         }
     });
 });
